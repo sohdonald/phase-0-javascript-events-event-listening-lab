67cbb8288c2529d605eab597b65a7ab708e8ef87
Donald Soh <sohdonald@gmail.com>
2023-05-08 16:24:04 -0400

    some bugs

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
+
+const input = document.getElementById('button');
+
 function addingEventListener() {
+
+    function clickAlert() {
+    alert('I was clicked!');
 }
+ 
+    input.addEventListener('click', clickAlert);
+
+}
+
+
+//putting this inside addingEventListener function passes test
+// but now the clickAlert is not working
+
+
+
+// const input = document.getElementById('button');
+// input.addEventListener('click', function() {
+//     alert('I was clicked!');
+// });
+
+
+//invokes clickAlert
+
+// const button2 = document.querySelector("#button2");
+// //now everything in button2 activate clickAlert
+// button2.addEventListener('click', clickAlert);
